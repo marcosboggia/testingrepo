db070bf1ebced2bd09c6e27d28a9a8225a4272af
Marcos Boggia <marcos_boggia@hotmail.com>
2023-03-04 03:32:23 -0300

    Update javafile3.java

--- javafile3.java
+++ javafile3.java
@@ -2,6 +2,6 @@ public class Message {
   int x = 5;
   public static void message(String[] args) {
     Message myObj = new Message();
-    System.out.println(myObj.x.a);
+    System.out.println(myObj.x.a.b);
   }
 }
